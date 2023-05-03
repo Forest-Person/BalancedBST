5a15516a64f65db56c563e205666eae98bae7e12
matthew <permahuman@gmail.com>
2023-05-03 07:38:53 -0700

    Remove two method BST insert and finish single method recursive BST.

--- script.js
+++ script.js
@@ -20,114 +20,69 @@ class BalancedBST {
     this.root = null
  }
 
- insert(value){
-
-   let newNode = new NodeTree(value)
-
-   
-
-    if(this.root === null){
-       this.root = newNode
-       return this.root
+ sortedToBST(arr,start,end) {
         
-    }else{
-       return this.insertNode(this.root,newNode)
-    }
+    arr = arr.sort((a,b)=>a-b)
+
+    let newArr = [...arr]
+
+    if(start > end){return null}
+
+    let mid  = Math.round((start + end) / 2);
+
+    let node = new NodeTree(newArr[mid])
+
+    node.left = this.sortedToBST(newArr, start, mid -1)
+
+    node.right = this.sortedToBST(newArr, mid + 1, end)
+
+    this.root = node
     
-    
-
-
-
-
- }
-
-    insertNode(root,newNode){
-
-        if(newNode.value < root.value){
-            if(root.left === null){
-                root.left = newNode
-            }else{return this.insertNode(root.left,newNode)}
-
-        }
-
-        if(newNode.value > root.value){
-            if(root.right === null){
-                root.right = newNode
-            }else{return this.insertNode(root.right,newNode)}
-        }
-return root
-
+    return this.root
 
     }
 
-    insertion(val,root){
-
-        if(this.root === null){
-            this.root = new NodeTree(val)
-            
-        }
-         if(val < this.root.value){
-            if(this.root.left === null){
-           this.root.left = new NodeTree(val)
-           
-         }else{this.insertion(val,this.root.left)}
-       
-         }
-       
-    
-    }
-
-
-    sortedToBST(arr,start,end) {
+    insertion(val, node = this.root){
+      
+      if(this.root === null){
+        this.root = new NodeTree(val)
+        return this.root
+      }
+      
+      if(val < node.value){
+        console.log('Going Left!')
+        if(node.left === null){
+          node.left = new NodeTree(val)
+          
+        }else{return this.insertion(val,node.left)}
         
-            arr = arr.sort((a,b)=>a-b)
+      }
+     else{
+        if(node.right === null){
+          node.right = new NodeTree(val)
+        }else{return this.insertion(val,node.right)}
+      }
+    
+    }
+  
+//end of class
+  }
+                
+                const balancedBST = new BalancedBST()
 
-            let newArr = [...arr]
-
-            if(start > end){return null}
-
-            let mid  = Math.round((start + end) / 2);
-
-            let node = new NodeTree(newArr[mid])
-
-            node.left = this.sortedToBST(newArr, start, mid -1)
-
-            node.right = this.sortedToBST(newArr, mid + 1, end)
-
-            this.root = node
-            
-            return this.root
-
-            }
-
-
-
-}
-
-
-const balancedBST = new BalancedBST()
-
-
-/*
-Compare the value with the root of the BST.
-
-If the value to be inserted is less than the root, move to the left subtree. 
-
-Otherwise, if the value is greater than the root, move to the right subtree.
-
-Continue this process, until we hit a leaf node.
-
-Now if the value is greater than the leaf, create a 
-left child of the leaf and insert the value. 
-
-Otherwise, if the value is greater than the leaf, 
-create a right child of the leaf and insert the value in the right child.
-*/
+                balancedBST.insertion(6)
+                balancedBST.insertion(5)
+                
+                balancedBST.insertion(2)
+                balancedBST.insertion(3)
+                balancedBST.insertion(355)
+                console.log(balancedBST.root)
 
 
 
+  
 
-  const prettyPrint = (node, prefix = '', isLeft = true) => { //Function for pretty printing nodetree array
+const prettyPrint = (node, prefix = '', isLeft = true) => { //Function for pretty printing nodetree array
     if (node === null) {
        return;
     }
@@ -140,17 +95,4 @@ create a right child of the leaf and insert the value in the right child.
     }
   }
 
-
-  /*console.log(balancedBST.insert(33))
-  console.log(balancedBST.insert(22))
-  console.log(balancedBST.insert(21))
-  console.log(balancedBST.insert(55))
-  console.log(balancedBST.insert(576))
-  prettyPrint(balancedBST.root)
- */
-
-  console.log(balancedBST.insertion(66))
-  console.log(balancedBST.insertion(3))
-  console.log(balancedBST.root)
-
-  //console.log(balancedBST.insertion(62))
+ prettyPrint(balancedBST.root)
